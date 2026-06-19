05f9f541cd07ddb3a697e05f1e78aecca9c94cbb
bukov80 <64306110+bukov80@users.noreply.github.com>
2026-06-19 11:08:59 +0200

    Update 180626.py

--- 180626.py
+++ 180626.py
@@ -1,22 +1,30 @@
 import sqlite3
 import pandas as pd
 
+
 conn = sqlite3.connect("online_store.db")
 
 query = """
 SELECT
     p.product_id,
     p.name AS product_name,
+    GROUP_CONCAT(DISTINCT w.name) AS warehouses,
     SUM(i.quantity) AS total_stock
-FROM inventory i
-JOIN products p
+FROM inventory AS i
+JOIN products AS p
     ON p.product_id = i.product_id
-JOIN warehouses w
+JOIN warehouses AS w
     ON w.warehouse_id = i.warehouse_id
-GROUP BY p.product_id, p.name
+WHERE p.is_active = 0
+GROUP BY
+    p.product_id,
+    p.name
+HAVING SUM(i.quantity) > 0
 ORDER BY total_stock DESC
 """
 
 df = pd.read_sql_query(query, conn)
 
-print(df.head(5))
+print(df.head())
+
+conn.close()
